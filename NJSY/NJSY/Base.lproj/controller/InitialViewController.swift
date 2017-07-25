bb5529e5d1f31af88154702f35caa2b1aae201e0
MasonMei666 <mason.mei97@gmail.com>
2017-07-25 02:29:07 -0700

    with error

---
+++ NJSY/NJSY/Base.lproj/controller/InitialViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GameViewController.swift
+//  NJSY
+//
+//  Created by Pxr on 2017/7/16.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class InitialViewController: UIViewController {
+    
+    private var userService:UserService!
+    
+    private var roomService:RoomService!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(jumpToSecondView))
+    
+        self.view.addGestureRecognizer(gesture)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    func jumpToSecondView(){
+        let destinationStoryboard = UIStoryboard(name:"Main", bundle: nil)
+        let destinationViewController = destinationStoryboard.instantiateViewController(withIdentifier: "SecondView")
+        self.present(destinationViewController, animated: true, completion: nil)
+    }
+
+}
