4250c96a3efae4cc3212d8299407285d159b8c61
Cflelf <664158315@qq.com>
2017-07-26 15:56:23 +0800

    pxr限制给mxr学习

--- NJSY/NJSY/Base.lproj/controller/SecondViewController.swift
+++ NJSY/NJSY/Base.lproj/controller/SecondViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
-import SpriteKit
+import SnapKit
 
 class SecondViewController: UIViewController {
     
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var SignUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        signInButton.snp.makeConstraints{(make) -> Void in
+            make.leading.equalTo(self.view.snp.leading).offset(self.view.bounds.width*0.27)
+            make.bottom.equalTo(self.view.snp.bottom).offset(-30)
+        }
     }
     
     override func didReceiveMemoryWarning() {
